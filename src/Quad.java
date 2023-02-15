8d47c06e45a1f2a41399ab48ca73115c2858d9a5
Ivan Petrov <ivannikopol86@gmail.com>
2023-02-15 18:19:45 +0300

    start

---
+++ src/Quad.java
@@ -0,0 +1,6 @@
+public class Quad extends Shape {
+    @Override
+    public void shapeName(String name) {
+        super.shapeName(name);
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,5 @@
+public class Shape {
+    public void shapeName(String name){
+        System.out.println("name = " + name);
+    }
+}
